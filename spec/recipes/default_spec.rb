5d533de4b24739b2ea6a53fb4d72d87bcb377a3b
Xabier de Zuazo <xabier@onddo.com>
2014-07-12 14:28:35 +0200

    Added recipe and unit tests

---
+++ spec/recipes/default_spec.rb
@@ -0,0 +1,61 @@
+#
+# Author:: Xabier de Zuazo (<xabier@onddo.com>)
+# Copyright:: Copyright (c) 2014 Onddo Labs, SL. (www.onddo.com)
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+require 'swap_tuning'
+
+describe 'swap_tuning::default' do
+
+  shared_examples_for 'a machine in need of swap' do |memory|
+    let(:chef_run) do
+      ChefSpec::Runner.new do |node|
+        node.set['memory']['total'] = memory[:memory]
+        node.set['memory']['swap']['total'] = memory[:current_swap]
+      end.converge(described_recipe)
+    end
+
+    it 'should reload ohai memory plugin' do
+      expect(chef_run).to reload_ohai('reload_memory').with(
+        plugin: 'memory',
+      ).at_compile_time
+    end
+
+    it "should create a swap file of #{memory[:new_swap]} MB" do
+      expect(chef_run).to create_swap_file('/swapfile0').with(
+        size: memory[:new_swap]
+      )
+    end
+  end # shared_examples_for a machine in need of swap
+
+  describe 'with 1 GB memory and 0 GB swap' do
+    it_should_behave_like 'a machine in need of swap', {
+      :memory => system_memory(1 * GB),
+      :current_swap => 0,
+      :new_swap => swap_file_size(2 * GB),
+    }
+  end
+
+  describe 'with 3 GB memory and 1 GB swap' do
+    it_should_behave_like 'a machine in need of swap', {
+      :memory => system_memory(3 * GB),
+      :current_swap => system_swap(1 * GB),
+      :new_swap => swap_file_size(2 * GB),
+    }
+  end
+
+end
---
+++ spec/unit/swap_tuning_spec.rb
@@ -0,0 +1,94 @@
+#
+# Author:: Xabier de Zuazo (<xabier@onddo.com>)
+# Copyright:: Copyright (c) 2014 Onddo Labs, SL. (www.onddo.com)
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+require 'swap_tuning'
+
+describe Chef::SwapTuning do
+
+  describe '#memory2bytes' do
+
+    {
+      '256' => 256,
+      '256BAD' => 256,
+      '256KB' => 256 * KB,
+      '256MB' => 256 * MB,
+      '256GB' => 256 * GB,
+      '256TB' => 256 * TB,
+     }.each do |memory, bytes|
+
+        it "should convert #{memory} to #{bytes}" do
+          expect(described_class.memory2bytes(memory)).to eq(bytes)
+        end
+
+      end
+
+  end # describe #memory2bytes
+
+  describe '#recommended_size_bytes' do
+
+    {
+      '256MB'   => 512 * MB,
+      1.9 * GB  => 2 * 1.9 * GB,
+      2.1 * GB  => 2.1 * GB,
+      '4GB'     => 4 * GB,
+      7.9 * GB  => 7.9 * GB,
+      8.1 * GB  => 8.1 * GB / 2,
+      '10GB'    => 5 * GB,
+      '100GB'   => 50 * GB,
+    }.each do |memory, swap|
+      memory = memory.kind_of?(Numeric) ? memory.round : memory
+      swap = swap.floor
+
+      it "should recommend #{swap} swap size with #{memory} memory" do
+        allow(Chef::Log).to receive(:warn)
+        swap_diff = described_class.recommended_size_bytes(memory) - swap
+        expect(swap_diff.abs).to be < 5
+      end
+
+      if described_class.memory2bytes(memory) <= 64 * GB
+
+        it "should not print any warning if #{memory} RAM size is not too high" do
+          expect(Chef::Log).not_to receive(:warn)
+          described_class.recommended_size_bytes(memory)
+        end
+
+      else
+
+        it "should print a warning if #{memory} RAM size is too high" do
+          expect(Chef::Log).to receive(:warn).once
+          described_class.recommended_size_bytes(memory)
+        end
+
+      end
+
+    end # each do memory, swap
+
+  end # describe recommended_size_bytes
+
+  describe '#recommended_size_mb' do
+
+    it 'should return #recommended_size_bytes result in MB' do
+      memory = 4 * GB
+      expect(described_class).to receive(:recommended_size_bytes).once.with(memory).and_return(memory)
+      expect(described_class.recommended_size_mb(memory)).to eql(memory / (1024 * 1024).ceil)
+    end
+
+  end # describe #recommended_size_mb
+
+end
