544f8d70862d1a70db0b3c30a214f9799ad9f3f2
Xabier de Zuazo <xabier@onddo.com>
2014-07-14 14:23:07 +0200

    Fix: Missing utf-8 encoding comment.

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 name 'swap_tuning'
 maintainer 'Onddo Labs, Sl.'
 maintainer_email 'team@onddo.com'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 #
 # Cookbook Name:: swap_tuning
 # Recipe:: default
