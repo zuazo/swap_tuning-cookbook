6d21fdda133442180bf92575a68c3aeff79c94da
Xabier de Zuazo <xabier@onddo.com>
2014-07-14 13:29:36 +0200

    Added LICENSE file and headers

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: swap_tuning
 # Recipe:: default
 #
-# Copyright 2014, Onddo Labs, Sl.
+# Copyright 2014, Onddo Labs, SL.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
